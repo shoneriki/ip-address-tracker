b7f215854763dd078da5c050ca568123b9a5ac56
shoneriki <ikirenohs@gmail.com>
2022-07-09 11:46:46 +0900

    added leaflet.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ locationElement = document.getElementById('location');
 time = document.getElementById('time');
 isp = document.getElementById('isp');
 searchBtn = document.getElementById('search-btn');
+input = document.getElementById('input');
+
+success = (position) => {
+  lat = position.coords.latitude
+  long = position.coords.longitude
+  map = 
+}
