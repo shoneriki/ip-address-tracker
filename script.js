24d193e1f02d140b3c3781ebfcb0f90aa847c4b0
shoneriki <ikirenohs@gmail.com>
2022-07-09 10:58:57 +0900

    elements put in as id

---
+++ script.js
@@ -0,0 +1,5 @@
+ipElement = document.getElementById('ip');
+locationElement = document.getElementById('location');
+time = document.getElementById('time');
+isp = document.getElementById('isp');
+searchBtn = document.getElementById('search-btn');
