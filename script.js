e13ea21669caa5a05882420c2d039b14a181b7c9
shoneriki <ikirenohs@gmail.com>
2022-07-10 13:58:19 +0900

    html redo, header reformatted

--- script.js
+++ script.js
@@ -8,5 +8,25 @@ input = document.getElementById('input');
 success = (position) => {
   lat = position.coords.latitude
   long = position.coords.longitude
-  map = 
+  map = L.map('map').setView([lat, long], 10)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
+
+  L.marker([lat, long]).addTo(map)
+  console.log(long)
+  console.log(lat)
+  {
+    error= () => {
+      alert("We apologize, we were unable to fetch the location")
+    }
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error)
+
+  function ipSearch() {
+    
+  }
+
 }
